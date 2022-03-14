4b02a125177fbc7bb14c0ec522fc2b6d461ba30d
Raul Rosenlöcher <git@home>
2022-03-14 21:31:55 +0100

    typing added

--- src/hue/hue_event_converter.py
+++ src/hue/hue_event_converter.py
@@ -1,8 +1,11 @@
-from typing import Optional
+from typing import Optional, Union
 
 from aiohue.v2 import EventType
 from aiohue.v2.models.feature import OnFeature, DimmingFeature
+from aiohue.v2.models.grouped_light import GroupedLight
+from aiohue.v2.models.light import Light
 from aiohue.v2.models.resource import ResourceTypes
+from aiohue.v2.models.room import Room
 
 from src.thing.thing_event import ThingEvent, ThingStatus
 
@@ -11,7 +14,7 @@ class HueEventConverter:
 
     # noinspection PyShadowingBuiltins
     @classmethod
-    def to_thing_event(cls, event_type: EventType, item, name=None, type=None) -> ThingEvent:
+    def to_thing_event(cls, event_type: EventType, item: Union[Light, GroupedLight, Room], name=None, type=None) -> ThingEvent:
         e = ThingEvent(status=ThingStatus.ERROR)
 
         e.id = item.id
